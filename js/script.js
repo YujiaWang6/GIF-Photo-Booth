a14c758869f421b1ca3200bcacd3ac80017acdba
YujiaWang6 <wang.yujia111@gmail.com>
2023-04-18 10:43:35 -0400

    added animated theme 3

--- js/script.js
+++ js/script.js
@@ -3,20 +3,28 @@ function pageLoaded(){
     var switcherBar = document.getElementById("switcher");
     var theme1 = document.getElementById("theme1-night-with-stars");
     var theme2 = document.getElementById("theme2-colourful-dots");
+    var theme3 = document.getElementById("theme3-trianglecolorchange");
     var i = 0;
     switcherBar.onclick = changeTheme;
     function changeTheme(){
         i=i+1;
-        if(i>1){
+        if(i>2){
             i=0;
         }
         if(i===0){
             theme1.style.display = "block";
             theme2.style.display = "none";
+            theme3.style.display="none";
         }
         if(i===1){
             theme1.style.display = "none";
             theme2.style.display = "block";
+            theme3.style.display="none";
+        }
+        if(i===2){
+            theme1.style.display = "none";
+            theme2.style.display = "none";
+            theme3.style.display="block";
         }
     }
 }
